9d366b5e52c9173920d8f01cb14aeecd2037ab3a
Daniel Bravo <daniel.brv@hotmail.cl>
2022-01-25 16:41:40 -0300

    Se agrega a create.js ejemplo de agregar equipos

--- create.js
+++ create.js
@@ -12,60 +12,60 @@ async function atack() {
       database_id: databaseId,
     },
     properties: {
-      Nombre: {
-        title: [
+      "Nombre": {
+        "title": [
           {
-            text: {
-              content: "Tuscan Kale 2",
-            },
-          },
-        ],
+            "text": {
+              "content": "Tuscan Kale 2"
+            }
+          }
+        ]
       },
-      Marca: {
-        rich_text: [
+      "Marca": {
+        "rich_text": [
           {
-            text: {
-              content: "A dark green leafy vegetable",
-            },
-          },
-        ],
+            "text": {
+              "content": "A dark green leafy vegetable"
+            }
+          }
+        ]
       },
-      Modelo: {
-        rich_text: [
+      "Modelo": {
+        "rich_text": [
           {
-            text: {
-              content: "A dark green leafy vegetable",
-            },
-          },
-        ],
+            "text": {
+              "content": "A dark green leafy vegetable"
+            }
+          }
+        ]
       },
-      Patente: {
-        rich_text: [
+      "Patente": {
+        "rich_text": [
           {
-            text: {
-              content: "A dark green leafy vegetable",
-            },
-          },
-        ],
+            "text": {
+              "content": "A dark green leafy vegetable"
+            }
+          }
+        ]
       },
-      Año: {
-        number: 2020,
+      "Año": {
+        "number": 2020
       },
       "Tipo de equipo": {
-        select: {
-          name: "MAQUINARIAS",
-        },
+        "select": {
+          "name": "MAQUINARIAS"
+        }
       },
       "N° SERIE / N° VIN": {
-        rich_text: [
+        "rich_text": [
           {
-            text: {
-              content: " no null",
-            },
-          },
-        ],
-      },
-    },
+            "text": {
+              "content": " no null"
+            }
+          }
+        ]
+      }
+    }
   });
   console.log(response);
 }
